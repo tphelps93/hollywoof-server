a83060c6a8bfe373dc15ff35096df89536888462
Taylor Phelps <tphelps1993@gmail.com>
2020-12-31 13:35:34 -0500

    created tables

--- src/app.js
+++ src/app.js
@@ -3,19 +3,15 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+const { NODE_ENV, DATABASE_URL } = require('./config');
 
 const app = express();
 
-const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
